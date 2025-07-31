ae4add1a4f5d04f1f93189bbc8a95fef32f23e0b
duddits <davinciihethal@gmail.com>
2025-07-31 12:29:12 +0300

    Add heropage(basic)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,27 @@
-import { Routes, Route, Link } from 'react-router-dom'
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import Profile from './pages/Profile'
+import { Routes, Route, Link } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Hero from "./pages/Hero"; // <-- add hero page
 
 export default function App() {
   return (
     <div>
-      <nav>
-        <Link to="/signup">Signup</Link> |{" "}
-        <Link to="/login">Login</Link> |{" "}
+      {/* Simple Navigation */}
+      <nav className="p-4 bg-gray-800 text-white flex space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/signup">Signup</Link>
+        <Link to="/login">Login</Link>
         <Link to="/profile">Profile</Link>
       </nav>
+
+      {/* Routing */}
       <Routes>
+        <Route path="/" element={<Hero />} /> {/* Hero as homepage */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-  )
+  );
 }
